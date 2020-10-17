f651baa9547ce37777c508064e88d1564a75874c
zhaoj <zhao602830422@163.com>
2020-10-17 15:05:04 +0800

    第一次添加第二个方法

---
+++ src/test/java/com/zhaoj/git_test/AG.java
@@ -0,0 +1,27 @@
+package com.zhaoj.git_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AG {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("你好");
+
+        System.out.println("你好");
+    }
+
+    @Test
+    public void contextLoads1() {
+        System.out.println("你好");
+
+        System.out.println("你好");
+    }
+
+
+}
